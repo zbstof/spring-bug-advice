1b7bc874e7b11b1ddcac08b7e1fba2eeb2dc1745
Stanislav Bytsko <zbstof@gmail.com>
2018-05-26 09:44:58 +0300

    Simplifications

--- src/test/java/com/example/demo/AdviceTests.java
+++ src/test/java/com/example/demo/AdviceTests.java
@@ -2,21 +2,14 @@ package com.example.demo;
 
 import lombok.extern.log4j.Log4j2;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.core.NestedExceptionUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,38 +18,112 @@ import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
+import static org.springframework.core.NestedExceptionUtils.getMostSpecificCause;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SuppressWarnings("unused")
 @Log4j2
 public class AdviceTests {
 
+    private static final String ERROR = "X-ERROR";
+
     @RunWith(SpringRunner.class)
     @SpringBootTest
-    @ContextConfiguration(classes = StuffITest.class)
+    public abstract static class Base {
+        @Autowired
+        protected WebApplicationContext webApplicationContext;
+
+        @Test
+        public void test() throws Exception {
+            MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+                    .perform(get("/"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(header().string(ERROR, Application.EXPECTED_ERROR));
+        }
+    }
+
+    private static String getOriginal(Exception exception) {
+        return ((JsonMappingException) getMostSpecificCause(exception)).getOriginalMessage();
+    }
+
+    @ContextConfiguration(classes = SingleAdviceTestExceptionTypesInAnnotationParameters.class)
     @TestConfiguration
-    public static class StuffITest {
+    public static class SingleAdviceTestExceptionTypesInAnnotationParameters extends Base {
 
         @ControllerAdvice
         public class Advice {
             @ExceptionHandler( {JsonMappingException.class, MismatchedInputException.class})
-            public ResponseEntity<Void> handleJsonWithGeneric(Exception exception) {
+            public ResponseEntity handleJsonWithGeneric(Exception exception) {
                 log.warn("handleJsonWithGeneric", exception);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).header("X-ERROR",
-                        ((JsonMappingException) NestedExceptionUtils.getMostSpecificCause(exception)).getOriginalMessage()).build();
+                return ResponseEntity.status(BAD_REQUEST).header(ERROR, getOriginal(exception)).build();
             }
         }
+    }
 
-        @Autowired
-        private WebApplicationContext webApplicationContext;
+    @ContextConfiguration(classes = SingleAdviceTestExceptionInMethodSignature.class)
+    @TestConfiguration
+    public static class SingleAdviceTestExceptionInMethodSignature extends Base {
 
-        @Test
-        public void shouldNotPatchIncorrectFields() throws Exception {
-            MockHttpServletResponse response = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
-                    .perform(MockMvcRequestBuilders.get("/"))
-                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                    .andReturn()
-                    .getResponse();
-            Assert.assertEquals(response.getHeader("X-ERROR"),
-                    "Cannot deserialize instance of `java.lang.String` out of START_OBJECT token");
+        @ControllerAdvice
+        public class Advice {
+            @ExceptionHandler
+            public ResponseEntity handleJsonWithGeneric(JsonMappingException exception) {
+                log.warn("handleJsonWithGeneric", exception);
+                return ResponseEntity.status(BAD_REQUEST).header(ERROR, exception.getOriginalMessage()).build();
+            }
+        }
+    }
+
+    @ContextConfiguration(classes = SingleAdviceTestExplicitGenericExceptionHandlerWithAdditionalIaeHandler.class)
+    @TestConfiguration
+    public static class SingleAdviceTestExplicitGenericExceptionHandlerWithAdditionalIaeHandler extends Base {
+
+        @ControllerAdvice
+        public class Advice {
+            @ExceptionHandler( {JsonMappingException.class, MismatchedInputException.class})
+            public ResponseEntity handleJsonWithGeneric(Exception exception) {
+                log.warn("handleJsonWithGeneric", exception);
+                return ResponseEntity.status(BAD_REQUEST).header(ERROR, getOriginal(exception)).build();
+            }
+
+            @ExceptionHandler
+            public ResponseEntity handleGenericIEA(IllegalArgumentException exception) {
+                log.error("handleGeneric", exception);
+                return ResponseEntity.status(INTERNAL_SERVER_ERROR).header(ERROR,
+                        getMostSpecificCause(exception).getMessage()).build();
+            }
+
+            @ExceptionHandler
+            public ResponseEntity handleGeneric(Exception exception) {
+                log.error("handleGeneric", exception);
+                return ResponseEntity.status(INTERNAL_SERVER_ERROR).header(ERROR,
+                        getMostSpecificCause(exception).getMessage()).build();
+            }
+        }
+    }
+
+    @ContextConfiguration(classes = SingleAdviceTestExplicitGenericExceptionHandler.class)
+    @TestConfiguration
+    public static class SingleAdviceTestExplicitGenericExceptionHandler extends Base {
+
+        @ControllerAdvice
+        public class Advice {
+            @ExceptionHandler( {JsonMappingException.class, MismatchedInputException.class})
+            public ResponseEntity handleJsonWithGeneric(Exception exception) {
+                log.warn("handleJsonWithGeneric", exception);
+                return ResponseEntity.status(BAD_REQUEST).header(ERROR, getOriginal(exception)).build();
+            }
+
+            @ExceptionHandler
+            public ResponseEntity handleGeneric(Exception exception) {
+                log.error("handleGeneric", exception);
+                return ResponseEntity.status(INTERNAL_SERVER_ERROR).header(ERROR,
+                        getMostSpecificCause(exception).getMessage()).build();
+            }
         }
     }
 }
